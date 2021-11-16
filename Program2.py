defd960de9386af42295d86e719e14b05597998e
CarloLim <CarloLim01>
2021-11-16 21:13:28 +0800

    Program 2 (Adding the first, second and third number)

---
+++ Program2.py
@@ -0,0 +1,7 @@
+# Create a program that ask 3 numbers. 
+# Find the lowest number using only if-else statement.
+# Display the lowest number
+
+first = int(input("Enter first number: "))
+second = int(input("Enter second number: "))
+third = int(input("Enter third number: "))
