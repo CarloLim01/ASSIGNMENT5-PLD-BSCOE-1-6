50a7daec345464d78db80ffb789e3b92184e682f
CarloLim <CarloLim01>
2021-11-16 21:37:03 +0800

    Program 2 (Finalizing the input in Finding the lowest number) Done

--- Program2.py
+++ Program2.py
@@ -5,3 +5,11 @@
 first = int(input("Enter first number: "))
 second = int(input("Enter second number: "))
 third = int(input("Enter third number: "))
+
+if first < second and first < third:
+    print(f"The lowest number is {first}.")
+elif second < first and second < third:
+    print(f"The lowest number is {second}.")
+else:
+    print(f"The lowest number is {third}.")
+    
